563e839775b05dcb52d409f88f6d39bcb6aa2b7a
Justin.Wang <rongchao.wang@ele.me>
2016-12-20 22:33:50 +0800

    init start

--- ios/build/Build/Intermediates/RCTNetwork.build/Debug-iphonesimulator/RCTNetwork.build/Objects-normal/x86_64/RCTFileRequestHandler.d
+++ ios/build/Build/Intermediates/RCTNetwork.build/Debug-iphonesimulator/RCTNetwork.build/Objects-normal/x86_64/RCTFileRequestHandler.d
@@ -1,8 +1,5 @@
 dependencies: \
-  /Users/wangrongchao/Desktop/RN/MeiTuanDemo/node_modules/react-native/Libraries/Network/RCTFileRequestHandler.m \
-  /Users/wangrongchao/Desktop/RN/MeiTuanDemo/node_modules/react-native/Libraries/Network/RCTFileRequestHandler.h \
+  /Users/wangrongchao/MeiTuanDemo/node_modules/react-native/Libraries/Network/RCTFileRequestHandler.m \
+  /Users/wangrongchao/MeiTuanDemo/node_modules/react-native/Libraries/Network/RCTFileRequestHandler.h \
   ../../React/Base/RCTURLRequestHandler.h \
-  ../../React/Base/RCTBridgeModule.h ../../React/Base/RCTDefines.h \
-  ../../React/Base/RCTURLRequestDelegate.h \
-  ../../React/Base/RCTInvalidating.h ../../React/Base/RCTUtils.h \
-  ../../React/Base/RCTAssert.h
+  ../../React/Base/RCTBridgeModule.h
--- ios/build/Build/Intermediates/RCTVibration.build/Debug-iphonesimulator/RCTVibration.build/StaticAnalyzer/RCTVibration/RCTVibration/normal/x86_64/RCTVibration.d
+++ ios/build/Build/Intermediates/RCTVibration.build/Debug-iphonesimulator/RCTVibration.build/StaticAnalyzer/RCTVibration/RCTVibration/normal/x86_64/RCTVibration.d
@@ -1,4 +1,4 @@
 dependencies: \
-  /Users/wangrongchao/Desktop/RN/MeiTuanDemo/node_modules/react-native/Libraries/Vibration/RCTVibration.m \
-  /Users/wangrongchao/Desktop/RN/MeiTuanDemo/node_modules/react-native/Libraries/Vibration/RCTVibration.h \
-  ../../React/Base/RCTBridgeModule.h ../../React/Base/RCTDefines.h
+  /Users/wangrongchao/MeiTuanDemo/node_modules/react-native/Libraries/Vibration/RCTVibration.m \
+  /Users/wangrongchao/MeiTuanDemo/node_modules/react-native/Libraries/Vibration/RCTVibration.h \
+  ../../React/Base/RCTBridgeModule.h
