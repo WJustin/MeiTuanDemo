1097d4962522eccfb5d7fc46c4c3d4bb7df3020b
Justin.Wang <rongchao.wang@ele.me>
2016-12-24 21:28:39 +0800

    添加网络请求

--- app/components/home.js
+++ app/components/home.js
@@ -14,21 +14,34 @@ import {
   Image,
   TextInput,
   ScrollView,
+  RefreshControl,
+  Alert,
 } from 'react-native';
 import HomeDetail from './homeDetail';
 import HomeEntryView from './homeEntryView';
 import HomeShopCenter from './homeShopCenter'
+import HomeGuessYouLikeView from './homeGuessYouLikeView'
+import * as ServerApi from '../constants/serverApi'
 import Dimension from 'Dimensions';
 const {width,height} = Dimension.get('window');
 
 export default class Home extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dataArray : [],
+      isRefresh: false,
+    };
+  }
   render() {
     return (
       <View style={styles.container}>
         {this._renderNavigationBar()}
-        <ScrollView >
+        <ScrollView refreshControl={<RefreshControl refreshing={this.state.isRefresh}
+                                                    onRefresh={()=>this._onRefresh()}/>}>
           <HomeEntryView style={styles.entryView}/>
           <HomeShopCenter/>
+          <HomeGuessYouLikeView dataArray={this.state.dataArray}/>
         </ScrollView>
       </View>
     );
@@ -37,9 +50,7 @@ export default class Home extends Component {
   _renderNavigationBar(){
     return(
       <View style={styles.navigationBar}>
-        <Text>
-           上海
-        </Text>
+        <Text>上海</Text>
         <TextInput placeholder='  请输入商家' style={styles.navigationBarTextInput}/>
         <View style={styles.navigationBarRight}>
           <Image source={{uri:'icon_homepage_message'}} style={styles.navigationBarRightImage}/>
@@ -54,6 +65,15 @@ export default class Home extends Component {
       component : HomeDetail,
       title     : '商品详情页'
     });
+  }
+
+  _onRefresh() {
+    this.setState({isRefresh: true});
+    fetch(ServerApi.API_GUESS_YOU_LIKE)
+        .then((response) => response.json())
+        .then((json)=>{
+          this.setState({dataArray:json.data, isRefresh: false});
+        })
   }
 }
 
---
+++ app/components/homeGuessYouLikeView.js
@@ -0,0 +1,79 @@
+/**
+ * Created by wangrongchao on 16/12/24.
+ */
+
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    ScrollView,
+    ListView,
+    Image,
+    Alert,
+} from 'react-native';
+import HomeSectionHeaderView from './homeSectionHeaderView'
+import Dimensions from 'Dimensions';
+const screenWidth =  Dimensions.get('window').width;
+const itemsPerRow = 5;
+const cellWidth = screenWidth / itemsPerRow;
+
+export default class HomeGuessYouLikeView extends Component {
+    render() {
+        let ds = new ListView.DataSource({rowHasChanged:(r1, r2)=>r1 !== r2});
+        return (
+            <View style={styles.container}>
+                <HomeSectionHeaderView leftTitle='猜你喜欢' rightTitle=''/>
+                <ListView dataSource={ds.cloneWithRows(this.props.dataArray)}
+                          renderRow={this._renderRows}
+                          enableEmptySections={true}/>
+            </View>
+        );
+    }
+
+    _renderRows(rowData) {
+        return (
+            <View style={styles.cell}>
+                <Image source={{uri:rowData.imageUrl}} style={styles.image}/>
+                <Text style={styles.text}>{rowData.title}</Text>
+            </View>
+        )
+    }
+}
+
+HomeGuessYouLikeView.propTypes = {
+    dataArray: React.PropTypes.array,
+};
+
+HomeGuessYouLikeView.defaultProps = {
+    dataArray : [],
+};
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop:10,
+    },
+
+    cell:{
+        flexDirection:'row',
+        backgroundColor:'white',
+        alignItems: 'center',
+        height:60,
+        padding:10,
+        borderBottomWidth : 0.5,
+        borderBottomColor : '#dddddd',
+    },
+
+    image: {
+        width:40,
+        height:40,
+        backgroundColor:'#dddddd'
+    },
+
+    text: {
+        fontSize: 17,
+        marginLeft:10,
+    },
+});
